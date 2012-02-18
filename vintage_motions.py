7e5376fa2e96e13229d5e01ab0fa25469d99a686
Erik Behrends <erik.behrends@gmail.com>
2012-02-18 13:19:44 +0100

    Remove unused arguments

--- vintage_motions.py
+++ vintage_motions.py
@@ -273,7 +273,7 @@ class ViExpandToBrackets(sublime_plugin.TextCommand):
             self.view.run_command('expand_selection', {'to': 'brackets', 'brackets': character})
 
 class ViExpandToParagraph(sublime_plugin.TextCommand):
-    def run(self, edit, outer = False, repeat = 1):
+    def run(self, edit):
         self.view.run_command('expand_selection_to_paragraph')
 
 class ScrollCurrentLineToScreenTop(sublime_plugin.TextCommand):
